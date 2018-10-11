68d9f6a7f11f5db90623b1de3520dd7f497bb40e
tangwei666 <18670055879@163.com>
2018-10-11 15:20:07 +0800

    Fix nothing except add array type

--- src/SettingController.php
+++ src/SettingController.php
@@ -63,6 +63,18 @@ class SettingController
     }
 
     /**
+     * show interface.
+     *
+     * @param $id
+     *
+     * @return Content
+     */
+    public function show($id)
+    {
+        return redirect()->route('setting.edit', ['id' => $id]);
+    }
+
+    /**
      * Edit interface.
      *
      * @param $id
@@ -74,21 +86,56 @@ class SettingController
         return Admin::content(function (Content $content) use ($id) {
             $content->header('header');
             $content->description('description');
-            $content->body($this->form()->edit($id));
+            $settings = SystemSetting::find($id);
+            $content->body($this->form($settings)->edit($id));
         });
     }
 
     /**
      * @return Form
      */
-    public function form()
+    public function form($settings = null)
     {
-        return Admin::form(SystemSetting::class, function (Form $form) {
+        return Admin::form(SystemSetting::class, function (Form $form) use ($settings) {
+
+            $type = request()->input('type') ? request()->input('type') : 0;
+            $value_edit = request()->input('value_edit') ? request()->input('value_edit') : '';
+            $value = request()->input('value') ? request()->input('value') : '';
+            $form->ignore(['type', 'value_edit']);
             $form->display('id', 'ID');
             $form->text('key')->rules('required');
-            $form->textarea('value')->rules('required');
+
+            if (!$settings) {
+                $form->textarea('value')->rules('required');
+
+            } else {
+                $value_old = $settings->value;
+
+                if (is_array($settings->value)) {
+                    $type = 1;
+                    $value_old = json_encode($value_old, true);
+                }
+
+                $form->textarea('value_edit', 'Value')->rules('required')->value($value_old);
+            }
+
+            $input_value = $value_edit ? $value_edit : $value;
+
+            $form->radio('type', 'Type')
+                ->options(['1' => 'array', '0' => 'string'])->default($type);
+
             $form->display('created_at');
             $form->display('updated_at');
+
+            $form->saved(function (Form $form) use ($input_value) {
+                $key = $form->input('key');
+                if (request()->input('type') == 1) {
+                    $input_value = json_decode($input_value, true);
+                }
+
+                settings([$key => $input_value]);
+            });
+
         });
     }
 
@@ -105,4 +152,6 @@ class SettingController
             $content->body($this->form());
         });
     }
+
+
 }
